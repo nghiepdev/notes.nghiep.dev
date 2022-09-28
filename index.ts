489698672d4cc885c26f88dfe5115dc025f5a6a2
nghiepit <me@nghiep.dev>
2022-09-28 17:37:20 +0700

    Create post note

--- index.ts
+++ index.ts
@@ -1,14 +1,50 @@
 import Fastify from 'fastify';
+import {Deta} from 'deta';
 
 const __PORT = process.env.PORT || 3000;
 const __DEV = process.env.NODE_ENV === 'development';
+
+interface Note {
+  key: string;
+  value: string;
+}
 
 const app = Fastify({
   bodyLimit: 100 * 1024 * 1024, // 100MiB
 });
 
-app.get('/', async (req, res) => {
+const deta = Deta(process.env.APP_DETA_PROJECT_KEY);
+const db = deta.Base(process.env.APP_NAME);
+
+app.get('/', async (request, reply) => {
+  // TODO: Build UI
   return {hello: 'world!'};
+});
+
+app.get<{
+  Params: {
+    id: string;
+  };
+}>('/:id', async (request, reply) => {
+  const note = (await db.get(request.params.id)) as unknown as Note;
+
+  if (note) {
+    return note.value;
+  }
+
+  reply.status(404).send('Note not found!');
+});
+
+app.post<{
+  Body: string;
+}>('/', async (request, reply) => {
+  const result = (await db.put(request.body)) as unknown as Note;
+
+  reply.headers({
+    Location: `/${result.key}`,
+  });
+
+  reply.send(result);
 });
 
 const start = async () => {
--- process.d.ts
+++ process.d.ts
@@ -1,5 +1,7 @@
 declare namespace NodeJS {
   interface ProcessEnv {
     NODE_ENV: 'production' | 'development';
+    APP_NAME: string;
+    APP_DETA_PROJECT_KEY: string;
   }
 }
