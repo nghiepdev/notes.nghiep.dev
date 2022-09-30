f4e1436d5f83b9c14489f8e73d1dd66dbdbd6f49
Nghiep <me@nghiep.dev>
2022-09-30 11:36:37 +0700

    Add prettier plugin

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  singleQuote: true,
+  trailingComma: 'all',
+  bracketSpacing: false,
+  arrowParens: 'avoid',
+  endOfLine: 'auto',
+  htmlWhitespaceSensitivity: 'ignore',
+  plugins: [require('prettier-plugin-tailwindcss')],
+};
