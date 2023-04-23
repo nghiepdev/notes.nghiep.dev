1a7c7ef18fe8db156eda140793c9528175ffaaab
Nghiep <me@nghiep.dev>
2023-04-23 16:03:27 +0700

    Improvements

--- prettier.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  singleQuote: true,
-  trailingComma: 'all',
-  bracketSpacing: false,
-  arrowParens: 'avoid',
-  endOfLine: 'auto',
-  htmlWhitespaceSensitivity: 'ignore',
-  plugins: [require('prettier-plugin-tailwindcss')],
-};
